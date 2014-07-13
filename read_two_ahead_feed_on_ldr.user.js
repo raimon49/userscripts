abeb4e5bcc78474d3024c64e10191201f1439661
raimon <raimon49@hotmail.com>
2014-07-13 19:23:45 +0900

    Support Greasemonkey 2.0 and later

--- read_two_ahead_feed_on_ldr.user.js
+++ read_two_ahead_feed_on_ldr.user.js
@@ -3,43 +3,61 @@
 // @description フィードを1つ飛ばしで読み進めるためのキーを追加するスクリプト
 // @namespace   http://sangoukan.xrea.jp/
 // @include     http://reader.livedoor.com/reader/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/read_two_ahead_feed_on_ldr.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/read_two_ahead_feed_on_ldr.user.js
 // @author      raimon
-// @version     1.0.0
+// @version     1.1.0
 // ==/UserScript==
 (function() {
-    var w = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
-
-    var KEY = 'S';
-
-    var State = w.State, Ordered = w.Ordered, Control = w.Control;
-    var touch = w.touch;
-    // via http://reader.livedoor.com/js/reader_main.0.3.4.js
-    var get_feed = function() {
-        var sid = State.now_reading;
-        if (!sid && Ordered.list) {
-            return Ordered.list[1];
+    function contentEval(source) {
+        if ("function" === typeof source) {
+            source = "(" + source + ")();"
         }
 
-        var list = Ordered.list;
-        if (!list) return;
+        var script = document.createElement("script");
+        script.setAttribute("type", "application/javascript");
+        script.textContent = source;
 
-        var offset = list.indexOfStr(sid);
-        var feed = list[offset+2];
-        return feed;
+        document.body.appendChild(script);
+        document.body.removeChild(script);
+    }
+
+    var init = function() {
+        const KEY = 'S';
+        var w = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
+        var State = w.State, Ordered = w.Ordered, Control = w.Control;
+        var touch = w.touch;
+        // via http://reader.livedoor.com/js/reader_main.0.3.4.js
+        var get_feed = function() {
+            var sid = State.now_reading;
+            if (!sid && Ordered.list) {
+                return Ordered.list[1];
+            }
+
+            var list = Ordered.list;
+            if (!list) return;
+
+            var offset = list.indexOfStr(sid);
+            var feed = list[offset+2];
+            return feed;
+        };
+
+        var read_two_ahead_subs = function() {
+            if (State.requested) return;
+
+            var feed = get_feed();
+            if (!feed) return;
+
+            State.requested = true;
+            touch(State.now_reading, "onclose");
+            Control.read(feed, Control.prefetch);
+        };
+
+        w.register_hook('AFTER_INIT', function() {
+            w.Keybind.add(KEY, read_two_ahead_subs);
+        });
     };
 
-    var read_two_ahead_subs = function() {
-        if (State.requested) return;
-
-        var feed = get_feed();
-        if (!feed) return;
-
-        State.requested = true;
-        touch(State.now_reading, "onclose");
-        Control.read(feed, Control.prefetch);
-    };
-
-    w.register_hook('AFTER_INIT', function() {
-        w.Keybind.add(KEY, read_two_ahead_subs);
-    });
+    contentEval(init);
 })();
--- show_time_on_ldr.user.js
+++ show_time_on_ldr.user.js
@@ -3,65 +3,84 @@
 // @namespace   http://sangoukan.xrea.jp/
 // @description Tキーで中の人に現在時刻と経過時間を表示してもらうスクリプト
 // @include     http://reader.livedoor.com/reader/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/show_time_on_ldr.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/show_time_on_ldr.user.js
 // @author      raimon
-// @version     1.0.0
+// @version     1.1.0
 // ==/UserScript==
 (function() {
-    var w = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
-    const SHOW_KEY = 't';
+    function contentEval(source) {
+        if ("function" === typeof source) {
+            source = "(" + source + ")();"
+        }
 
-    function Clock() {
-        this.start_time = new Date().getTime();
+        var script = document.createElement("script");
+        script.setAttribute("type", "application/javascript");
+        script.textContent = source;
+
+        document.body.appendChild(script);
+        document.body.removeChild(script);
+    }
+
+    var init = function() {
+        const SHOW_KEY = "t";
+        var w = window;
+        function Clock() {
+            this.start_time = new Date().getTime();
+        }
+
+        Clock.prototype.showTime = function() {
+            var now = new Date();
+            var hms = [];
+            [
+                now.getHours(),
+                now.getMinutes(),
+                now.getSeconds()
+            ].forEach(
+                function(item) {
+                    hms.push(this.format(item));
+                },
+                this
+            );
+
+            return hms.join(":");
+        }
+
+        Clock.prototype.showPassage = function() {
+            var passage_time = parseInt((new Date().getTime() - this.start_time) / 1000);
+            var hms = [];
+            [
+                parseInt(passage_time / (60 * 60)),
+                parseInt((passage_time / 60) % 60),
+                parseInt(passage_time % 60)
+            ].forEach(
+                function(item) {
+                    hms.push(this.format(item));
+                },
+                this
+            );
+
+            return hms.join(":");
+        };
+
+        Clock.prototype.format = function(t) {
+            return String("00" + t).substr(-2);
+        };
+
+        w.register_hook('AFTER_INIT', function() {
+            var c = new Clock();
+            w.Keybind.add(SHOW_KEY, function() {
+                var m = [
+                    "現在時刻: ",
+                    c.showTime(),
+                    "  経過時間: ",
+                    c.showPassage()
+                ].join("");
+                w.message(m);
+            });
+        });
     };
 
-    Clock.prototype.showTime = function() {
-        var now = new Date();
-        var hms = [];
-        [
-            now.getHours(),
-            now.getMinutes(),
-            now.getSeconds()
-        ].forEach(
-            function(item) {
-                hms.push(this.format(item));
-            },
-            this
-        );
-
-        return hms.join(":");
-    }
-
-    Clock.prototype.showPassage = function() {
-        var passage_time = parseInt((new Date().getTime() - this.start_time) / 1000);
-        var hms = [];
-        [
-            parseInt(passage_time / (60 * 60)),
-            parseInt((passage_time / 60) % 60),
-            parseInt(passage_time % 60)
-        ].forEach(
-            function(item) {
-                hms.push(this.format(item));
-            },
-            this
-        );
-
-        return hms.join(":");
-    }
-
-    Clock.prototype.format = function(t) {
-        return String("00" + t).substr(-2);
-    }
-
-    w.register_hook('AFTER_INIT', function() {
-        var c = new Clock();
-        w.Keybind.add(SHOW_KEY, function() {
-            var m = [
-                "現在時刻: ",
-                c.showTime(),
-                "  経過時間: ",
-                c.showPassage()
-            ].join("");
-            w.message(m);
-        });
-    });
+    contentEval(init);
 })();
