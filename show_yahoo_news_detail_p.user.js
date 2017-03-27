8db1fff9ba7e2113a2a632c23f5e436824d25591
raimon <raimon49@hotmail.com>
2017-03-27 17:40:53 +0900

    Support hosted by HTTPS page and add meta data

--- show_yahoo_news_detail_p.user.js
+++ show_yahoo_news_detail_p.user.js
@@ -3,9 +3,14 @@
 // @namespace   http://sangoukan.xrea.jp/
 // @description ヤフーニュースのヘッドラインを表示した時に自動で記事全文へ移動するスクリプト
 // @include     http://dailynews.yahoo.co.jp/fc/*
+// @include     https://dailynews.yahoo.co.jp/fc/*
 // @include     http://news.yahoo.co.jp/pickup/*
+// @include     https://news.yahoo.co.jp/pickup/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/show_yahoo_news_detail_p.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/show_yahoo_news_detail_p.user.js
 // @author      raimon
-// @version     1.1.3
+// @version     1.2.0
 // ==/UserScript==
 (function() {
     const SELECTOR_PATTERNS = [
