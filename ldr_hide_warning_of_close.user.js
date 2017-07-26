1703830cd7be1d2a996101a609fb7695d94c1829
raimon <raimon49@hotmail.com>
2017-07-26 22:12:16 +0900

    Goodbye, LDR

---
+++ ldr_hide_warning_of_close.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        LDR Hide Warning Of Close
+// @description LDRサービス終了のお知らせを隠すスクリプト
+// @namespace   http://sangoukan.xrea.jp/
+// @include     http://reader.livedwango.com/reader/*
+// @include     https://reader.livedwango.com/reader/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/ldr_hide_warning_of_close.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/ldr_hide_warning_of_close.user.js
+// @author      raimon
+// @version     1.0.0
+// ==/UserScript==
+(function() {
+    var blocks = document.querySelectorAll("body > div"),
+        isHide = false;
+    Array.from(blocks).forEach(function(div) {
+        if (isHide) {
+            return;
+        }
+
+        div.style.display = "none";
+        isHide = true;
+    });
+})();
+
