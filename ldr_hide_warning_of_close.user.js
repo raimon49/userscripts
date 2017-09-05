fc34592614b0fa2a7812a5c2465489051b4e375f
raimon <raimon49@hotmail.com>
2017-09-05 23:09:38 +0900

    Add include domain on 'livedoor.com'
    Bump version to 1.1.0

--- ldr_hide_warning_of_close.user.js
+++ ldr_hide_warning_of_close.user.js
@@ -2,13 +2,15 @@
 // @name        LDR Hide Warning Of Close
 // @description LDRサービス終了のお知らせを隠すスクリプト
 // @namespace   http://sangoukan.xrea.jp/
+// @include     http://reader.livedoor.com/reader/*
+// @include     https://reader.livedoor.com/reader/*
 // @include     http://reader.livedwango.com/reader/*
 // @include     https://reader.livedwango.com/reader/*
 // @grant       none
 // @downloadURL https://github.com/raimon49/userscripts/raw/master/ldr_hide_warning_of_close.user.js
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/ldr_hide_warning_of_close.user.js
 // @author      raimon
-// @version     1.0.0
+// @version     1.1.0
 // ==/UserScript==
 (function() {
     var blocks = document.querySelectorAll("body > div"),
