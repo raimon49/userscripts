3329c0826c1060b2737f69a142d4312c0d5ab88a
raimon <raimon49@hotmail.com>
2022-11-15 17:34:44 +0900

    Migrate obsolete APIs

--- show_yahoo_news_detail_p.user.js
+++ show_yahoo_news_detail_p.user.js
@@ -2,15 +2,15 @@
 // @name        show Yahoo! News detail page
 // @namespace   http://sangoukan.xrea.jp/
 // @description ヤフーニュースのヘッドラインを表示した時に自動で記事全文へ移動するスクリプト
-// @include     http://dailynews.yahoo.co.jp/fc/*
-// @include     https://dailynews.yahoo.co.jp/fc/*
-// @include     http://news.yahoo.co.jp/pickup/*
-// @include     https://news.yahoo.co.jp/pickup/*
+// @match       http://dailynews.yahoo.co.jp/fc/*
+// @match       https://dailynews.yahoo.co.jp/fc/*
+// @match       http://news.yahoo.co.jp/pickup/*
+// @match       https://news.yahoo.co.jp/pickup/*
 // @grant       none
 // @downloadURL https://github.com/raimon49/userscripts/raw/master/show_yahoo_news_detail_p.user.js
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/show_yahoo_news_detail_p.user.js
 // @author      raimon
-// @version     1.5.0
+// @version     1.6.0
 // ==/UserScript==
 (function() {
     const SELECTOR_PATTERNS = [
