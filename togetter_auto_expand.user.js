9d63803a73f71ead92162ed037ac1bf60b92b81c
raimon <raimon49@hotmail.com>
2022-11-28 20:15:12 +0900

    Add a script for Togetter auto expand

---
+++ togetter_auto_expand.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Togetter auto expand
+// @description Togetterページの「この続きを読む」自動でクリック
+// @namespace   http://sangoukan.xrea.jp/
+// @match       https://togetter.com/li/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/togetter_auto_expand.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/togetter_auto_expand.user.js
+// @noframes
+// @author      raimon
+// @version     1.0.0
+// ==/UserScript==
+(() => {
+    const moreTweet = document.querySelector("div.more_tweet_box > button");
+    if (moreTweet && typeof moreTweet.onclick === "function") {
+        moreTweet.onclick();
+    }
+})();
