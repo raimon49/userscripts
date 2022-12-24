a074eb94e2b39def79387de38ff4e17cb62b7435
raimon <raimon49@hotmail.com>
2022-12-24 11:46:34 +0900

    Add a script for Twitter web hide analytics numbers

---
+++ twitter_web_hide_analytics_numbers.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Twitter web hide analytics numbers
+// @description Twitter web上で表示される表示回数らしき数字を隠す
+// @namespace   http://sangoukan.xrea.jp/
+// @match       https://twitter.com/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/twitter_web_hide_analytics_numbers.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/twitter_web_hide_analytics_numbers.user.js
+// @noframes
+// @author      raimon
+// @version     1.0.0
+// ==/UserScript==
+(() => {
+    const hideAnalyticsNumbersStyle = document.createElement("style");
+    hideAnalyticsNumbersStyle.innerHTML = `
+        a[href$="analytics"] span[data-testid="app-text-transition-container"] >
+        span {
+            display: none;
+        }
+    `;
+    document.head.appendChild(hideAnalyticsNumbersStyle);
+})();
