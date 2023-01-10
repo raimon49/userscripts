ffa1a3c7d6039646e2ab0b1c01c45ee5c4ef31ed
raimon <raimon49@hotmail.com>
2023-01-10 21:33:22 +0900

    Update selector of Twitter web at 2023.01

--- twitter_web_hide_analytics_numbers.user.js
+++ twitter_web_hide_analytics_numbers.user.js
@@ -8,13 +8,15 @@
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/twitter_web_hide_analytics_numbers.user.js
 // @noframes
 // @author      raimon
-// @version     1.0.0
+// @version     1.0.1
 // ==/UserScript==
 (() => {
     const hideAnalyticsNumbersStyle = document.createElement("style");
     hideAnalyticsNumbersStyle.innerHTML = `
         a[href$="analytics"] span[data-testid="app-text-transition-container"] >
-        span {
+        span,
+        span.r-1b43r93:nth-child(1) > span:nth-child(1)
+        {
             display: none;
         }
     `;
