30863c4828c03aedb9e75bbd38c112790c9dd7fb
raimon <raimon49@hotmail.com>
2023-02-22 22:03:44 +0900

    Remove 'span[aria-hidden="true"] +' from selector

--- twitter_web_hide_analytics_numbers.user.js
+++ twitter_web_hide_analytics_numbers.user.js
@@ -8,15 +8,14 @@
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/twitter_web_hide_analytics_numbers.user.js
 // @noframes
 // @author      raimon
-// @version     1.0.3
+// @version     1.0.4
 // ==/UserScript==
 (() => {
     const hideAnalyticsNumbersStyle = document.createElement("style");
     hideAnalyticsNumbersStyle.innerHTML = `
         a[href$="analytics"] span[data-testid="app-text-transition-container"] >
         span,
-        article[data-testid="tweet"] div[dir="ltr"] span[aria-hidden="true"] +
-        span span[data-testid="app-text-transition-container"] > span
+        article[data-testid="tweet"] div[dir="ltr"] span span[data-testid="app-text-transition-container"] > span
         {
             display: none;
         }
