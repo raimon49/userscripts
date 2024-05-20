eef3a274d9ca9d53f6e9d47efbfbd0ac74f5335d
raimon <raimon49@hotmail.com>
2024-05-21 00:03:51 +0900

    Delay script execution in SBI Securities

--- set_stock_title_on_sbi.user.js
+++ set_stock_title_on_sbi.user.js
@@ -8,19 +8,23 @@
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/set_stock_title_on_sbi.user.js
 // @noframes
 // @author      raimon
-// @version     0.1.0
+// @version     0.1.1
 // ==/UserScript==
 (() => {
-    const shownPage = document.querySelector("h2.head01").textContent;
-    if (!shownPage || shownPage !== "国内株式") {
-        return;
-    }
+    const main = () => {
+        const shownPage = document.querySelector("h2.head01").textContent;
+        if (!shownPage || shownPage !== "国内株式") {
+            return;
+        }
 
-    const stockTitle = document.querySelector("h3:has(span.fm01)").textContent,
-        selectedTab = document.querySelector("div.tab02T td.act").textContent,
-        currentTitle= document.title;
+        const stockTitle = document.querySelector("h3:has(span.fm01)").textContent,
+            selectedTab = document.querySelector("div.tab02T td.act").textContent,
+            currentTitle= document.title;
 
-    if (currentTitle === "国内株式｜SBI証券" && stockTitle && selectedTab) {
-        document.title = `${stockTitle}｜${selectedTab}｜${currentTitle}`;
-    }
+        if (currentTitle === "国内株式｜SBI証券" && stockTitle && selectedTab) {
+            document.title = `${stockTitle}｜${selectedTab}｜${currentTitle}`;
+        }
+    };
+
+    setTimeout(main, 1500/* ms */);
 })();
