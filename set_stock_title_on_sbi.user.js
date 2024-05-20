f7d61a97841a934a276896a5f0cb6c2f0c5e4a12
raimon <raimon49@hotmail.com>
2024-05-20 20:20:40 +0900

    SBI Securities is suck

---
+++ set_stock_title_on_sbi.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Set stock title on SBI
+// @description SBI証券の国内株式ページで表示中の銘柄をタイトルにセットする
+// @namespace   http://sangoukan.xrea.jp/
+// @match       https://site1.sbisec.co.jp/ETGate/*
+// @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/set_stock_title_on_sbi.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/set_stock_title_on_sbi.user.js
+// @noframes
+// @author      raimon
+// @version     0.1.0
+// ==/UserScript==
+(() => {
+    const shownPage = document.querySelector("h2.head01").textContent;
+    if (!shownPage || shownPage !== "国内株式") {
+        return;
+    }
+
+    const stockTitle = document.querySelector("h3:has(span.fm01)").textContent,
+          selectedTab = document.querySelector("div.tab02T td.act").textContent,
+          currentTitle= document.title;
+
+    if (currentTitle === "国内株式｜SBI証券" && stockTitle && selectedTab) {
+        document.title = `${stockTitle}｜${selectedTab}｜${currentTitle}`;
+    }
+})();
