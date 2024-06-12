ca5ab8a8d1347f94931ee56b5a65a5bccd8be64a
raimon <raimon49@hotmail.com>
2024-06-12 19:13:37 +0900

    Use constant variables in SBI

--- set_stock_title_on_sbi.user.js
+++ set_stock_title_on_sbi.user.js
@@ -9,12 +9,14 @@
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/set_stock_title_on_sbi.user.js
 // @noframes
 // @author      raimon
-// @version     0.1.4
+// @version     0.1.5
 // ==/UserScript==
 (() => {
     const main = () => {
-        const shownPage = document.querySelector("h2.head01").textContent;
-        if (!shownPage || shownPage !== "国内株式") {
+        const DEFAULT_PAGE_HEADER = "国内株式",
+            DEFAULT_PAGE_TITLE = "国内株式｜SBI証券",
+            shownPage = document.querySelector("h2.head01").textContent;
+        if (!shownPage || shownPage !== DEFAULT_PAGE_HEADER) {
             return;
         }
 
@@ -22,7 +24,7 @@
             selectedTab = document.querySelector("div.tab02T td.act").textContent,
             currentTitle= document.title;
 
-        if (currentTitle === "国内株式｜SBI証券" && stockTitle && selectedTab) {
+        if (currentTitle === DEFAULT_PAGE_TITLE && stockTitle && selectedTab) {
             document.title = `${stockTitle}｜${selectedTab}｜${currentTitle}`;
             clearInterval(timerID);
         }
