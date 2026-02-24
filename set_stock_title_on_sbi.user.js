e664caa87b5510144ae312b6e110b53dee30a2ea
raimon <raimon49@hotmail.com>
2026-02-24 17:30:48 +0900

    SBI証券の銘柄詳細ページがリニューアルされたのに対応
    https://site1.sbisec.co.jp/ETGate/?OutSide=on&getFlg=on&_ControlID=WPLETmgR001Control&_PageID=WPLETmgR001Mdtl30&_ActionID=DefaultAID&_DataStoreID=DSWPLETmgR001Control&burl=search_domestic&cat1=domestic&cat2=none&dir=info&file=domestic_info260213_renewal.html

--- set_stock_title_on_sbi.user.js
+++ set_stock_title_on_sbi.user.js
@@ -9,20 +9,20 @@
 // @updateURL   https://github.com/raimon49/userscripts/raw/master/set_stock_title_on_sbi.user.js
 // @noframes
 // @author      raimon
-// @version     1.0.0
+// @version     1.1.0
 // ==/UserScript==
 (() => {
     const main = () => {
         const DEFAULT_PAGE_HEADER = "国内株式",
             DEFAULT_PAGE_TITLE = "国内株式｜SBI証券",
-            shownPage = document.querySelector("h2.head01").textContent;
+            shownPage = document.querySelector("a[aria-current=true]").textContent;
         if (!shownPage || shownPage !== DEFAULT_PAGE_HEADER) {
             clearInterval(timerID);
             return;
         }
 
-        const stockTitle = document.querySelector("h3:has(span.fm01)").textContent,
-            selectedTab = document.querySelector("div.tab02T td.act").textContent,
+        const stockTitle = document.querySelector("div.stock-title h1 span").textContent,
+            selectedTab = document.querySelector("a[aria-selected=true]").textContent,
             currentTitle= document.title;
 
         if (currentTitle === DEFAULT_PAGE_TITLE && stockTitle && selectedTab) {
